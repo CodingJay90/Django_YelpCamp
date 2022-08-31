b0dd5a356262a6953daa360d16d87c399747f667
Faruq <CodingJay90>
2022-08-31 22:04:14 +0100

    feature/campground-create

--- yelpcamp/campgrounds/urls.py
+++ yelpcamp/campgrounds/urls.py
@@ -5,6 +5,7 @@ app_name = 'campgrounds'
 
 urlpatterns = [
     path("", views.campgrounds_list, name='home'),
+    path('add/', views.campground_add, name='add'),
     path('<str:id>/', views.campground_detail, name='detail'),
     path('edit/<str:id>/', views.campground_edit, name='edit'),
     path('delete/<str:id>/', views.campground_delete, name='delete'),
--- yelpcamp/campgrounds/views.py
+++ yelpcamp/campgrounds/views.py
@@ -4,6 +4,7 @@ from pymongo import MongoClient
 from django import template
 from bson.objectid import ObjectId
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 
 connection_string = "mongodb://localhost/yelpcamp_django"
 client = MongoClient(connection_string)
@@ -32,16 +33,6 @@ location_2 = {
     },
     'location': 'Tasmania',
 }
-register = template.Library()
-
-
-# @register.filter(name='get')
-# def get(d, k):
-#     return d.get(k, None)
-
-def private(dic, key):
-    print(key)
-    return 'dic[key]'
 
 
 def campgrounds_list(request):
@@ -49,7 +40,7 @@ def campgrounds_list(request):
     mapped_campgrounds = list(
         map(lambda x: {'id': x['_id'], **x}, campgrounds))
     # locations_collection.insert_many([location_1, location_2])
-    return render(request, "campgrounds/index.html", {'campgrounds': mapped_campgrounds, 'private': private})
+    return render(request, "campgrounds/index.html", {'campgrounds': mapped_campgrounds})
 
 
 def campground_detail(request, id):
@@ -87,3 +78,22 @@ def campground_delete(request, id):
     if request.method == 'POST':
         locations_collection.delete_one({'_id': ObjectId(id)})
         return redirect('campgrounds:home')
+
+
+@login_required(login_url="/accounts/login/")
+def campground_add(request):
+    if request.method == 'POST':
+        update_body = {
+            'name': request.POST['name'],
+            'image': request.POST['image'],
+            'description': request.POST['description'],
+            'cost': request.POST['cost'],
+            'location': request.POST['name'],
+            'author': {
+                'id': request.user.id,
+                'username': request.user.username
+            },
+        }
+        result = locations_collection.insert_one(update_body)
+        return redirect("campgrounds:detail", id=result.inserted_id)
+    return render(request, "campgrounds/add_campground.html")
